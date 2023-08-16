4052bf3ee9a75839d2b30549690caa8b09210b06
sanjay200k <137974709+sanjay200k@users.noreply.github.com>
2023-08-16 14:17:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,61 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.datasets import load_diabetes
+
+# Load the Diabetes dataset
+diabetes = load_diabetes()
+X = diabetes.data
+y = diabetes.target
+
+# Convert target variable to binary (1 for diabetes, 0 for non-diabetes)
+y = np.where(y >= 200, 1, 0)
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Initialize the Random Forest classifier
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+
+# Train the model on the training data
+model.fit(X_train, y_train)
+
+# Make predictions on the testing data
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+accuracy = accuracy_score(y_test, y_pred)
+report = classification_report(y_test, y_pred)
+conf_matrix = confusion_matrix(y_test, y_pred)
+
+print("Accuracy:", accuracy)
+print("Classification Report:\n", report)
+print("Confusion Matrix:\n", conf_matrix)
+
+# Visualize Confusion Matrix
+plt.figure(figsize=(8, 6))
+plt.imshow(conf_matrix, interpolation='nearest', cmap=plt.cm.Blues)
+plt.title("Confusion Matrix")
+plt.colorbar()
+plt.xticks([0, 1], ["Non-Diabetes", "Diabetes"])
+plt.yticks([0, 1], ["Non-Diabetes", "Diabetes"])
+plt.xlabel("Predicted Label")
+plt.ylabel("True Label")
+plt.show()
+
+# Visualize Feature Importance
+feature_names = diabetes.feature_names
+importances = model.feature_importances_
+sorted_indices = np.argsort(importances)
+
+plt.figure(figsize=(10, 6))
+plt.barh(range(len(importances)), importances[sorted_indices], align='center')
+plt.yticks(range(len(importances)), [feature_names[i] for i in sorted_indices])
+plt.xlabel("Feature Importance")
+plt.ylabel("Feature")
+plt.title("Feature Importance in Diabetes Prediction")
+plt.show()
 
